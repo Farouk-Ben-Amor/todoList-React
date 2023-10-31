632b71da521e395b4d0c3c34f32aed1176fdc68e
farouk <faaroukbnamor@gmail.com>
2023-10-31 14:46:20 +0100

    Done

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import './styles.css'
+
+export default function App() {
+  const [newItem,setNewItem] = useState("")
+  const [todos,setTodos] = useState([])
+  const handleSubmit = e => {
+  e.preventDefault()
+
+  setTodos(currentTodos => {
+    return [
+    ...currentTodos ,
+    {id : uuidv4(),title:newItem,completed:false },
+    ]
+    })
+  setNewItem("")}
+
+const toggleTodo = (id,completed) => {
+  setTodos(currentTodos => {
+    return currentTodos.map(todo => {
+      if(todo.id === id) {
+        return {...todo,completed}
+      }
+      return todo
+    })
+  })
+}
+
+return (
+<>
+
+  <form onSubmit={handleSubmit} className='new-item-form' >
+    <div className='form-row' >  
+      <label htmlFor="item">New item</label>
+      <input  value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id='item' />
+    </div>
+    <button className='btn' >Add</button>
+  </form>
+  <h1 className='header' >To DO List</h1>
+  <ul className='list' >
+    {todos.map((todo) => {
+      return(
+<li key={todo.id} >
+<label>
+<input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+{ todo.title}
+</label>
+<button className='btn btn-danger' >Delete</button>
+</li>
+  )
+    })}
+  </ul>
+</>
+)
+}
